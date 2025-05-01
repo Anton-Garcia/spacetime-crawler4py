2f4ad9998ee6870ebf3debc2fce0a8236163895b
Anthony Garcia <anthojg7@uci.edu>
2025-05-01 01:56:57 -0700

    Added main functionality; need to work on subdomain count still

---
+++ analysis.py
@@ -0,0 +1,90 @@
+from pathlib import Path
+import doctools
+
+folder_path = Path("text_content")
+STOPWORDS = [
+    "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at",
+    "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could",
+    "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for",
+    "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's",
+    "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm",
+    "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't",
+    "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours",
+    "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so",
+    "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's",
+    "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until",
+    "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when",
+    "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would",
+    "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"
+]
+
+#one file for each url; same number of files as number of urls downloaded
+total_files = 0;
+
+#variable to hold the length of the longest file, in words
+longest_file = 0;
+
+#dictionary to hold all words and their frequencies
+master_token_count = dict()
+
+#dictionary to hold all subdomains and their number of pages
+subdomains = dict()
+
+def file_reader():
+    #allows function to use the values of the global variables, not local ones
+    global total_files, longest_file, master_token_count, subdomains
+
+    for file in folder_path.iterdir():
+        #####FINDING THE NUMBER OF DOCUMENTS##### 
+        total_files += 1
+        
+        #####FINDING THE LONGEST DOCUMENT#####    
+
+        #feading the file directly into the tokenize method gives a list of 
+        #all tokens in the file
+        tokenized = doctools.tokenize(file)
+        length_of_file = len(tokenized)
+
+        #keep track of which file is the longest
+        if(length_of_file > longest_file):
+            longest_file = length_of_file
+
+        #####FINDING MOST COMMON WORDS######
+
+        #get the frequency of all tokens in the document, and add them to the master dictionary
+        document_token_count = doctools.computeWordFrequencies(tokenized)
+        dictionary_adder(master_token_count, document_token_count)
+
+        #####FINDING SUBDOMAIN PAGE COUNTS#####
+          
+        #When we turned the urls into files, we had to change the / to avoid bad filenames
+        #this retuns the name back to the original url
+        url_name = file.name.replace('_', '/');
+
+        
+#function to add two dictionaries together
+def dictionary_adder(dict1, dict2):
+    for key, value in dict2.items():
+        if key in dict1:
+            dict1[key] +=  value
+        
+        else:
+            dict1[key] = value
+    
+    return dict1
+
+#function to clean stopwords out of dictionary
+def dictionary_sanitizer(dictionary):
+    for word in list(dictionary):
+        if word in STOPWORDS:
+            del dictionary[word]
+
+
+
+if __name__ == "__main__":
+    file_reader()
+    dictionary_sanitizer(master_token_count)
+    print("Total number of files downloaded: ", total_files)
+    print("Length of the longest file: ", longest_file)
+    print("Top 50 most common words:")
+    doctools.frequency_print(master_token_count)
